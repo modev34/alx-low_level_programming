c4183a05600bb8b4aa84a04204c53f40770ecf53
modev34 <modev34@gmail.com>
2023-03-16 15:31:12 -0700

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	printf("Size of a char: %zu", sizeof(char));
 	printf("Size of a int: %zu", sizeof(int));
-	printf("Size of a long int: %zu", sizeof(long));
-	printf("Size of a long long int: %zu", sizeof(long long));
-	printf("Size of a float: %zu", sizeof(float));
+	printf("Size of a long int: %zu", sizeof(long int));
+	printf("Size of a long long int: %zu", sizeof(long long int));
+	printf("Size of a float: %zu", sizeof(double));
        	return (0);
 }
