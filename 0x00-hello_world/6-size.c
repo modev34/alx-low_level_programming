5d815837fc437f59890b385b3d77ccc3a0f5931a
modev34 <modev34@gmail.com>
2023-03-16 15:38:11 -0700

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %zu", sizeof(char));
-	printf("Size of a int: %zu", sizeof(int));
-	printf("Size of a long int: %zu", sizeof(long int));
-	printf("Size of a long long int: %zu", sizeof(long long int));
+	printf("Size of a int: %zu",(unsigned) sizeof(int));
+	printf("Size of a long int: %zu", (unsigned)sizeof(long int));
+	printf("Size of a long long int: %zu", (unsigned)sizeof(long long int));
 	printf("Size of a float: %zu", sizeof(double));
        	return (0);
 }
