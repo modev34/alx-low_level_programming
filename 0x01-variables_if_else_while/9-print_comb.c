3c7ffea8deb49534452a76976817bc763fdfa22b
modev34 <modev34@gmail.com>
2023-03-17 18:20:00 -0700

    t

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print 0 to 9 separated with commas, using putchar
+ *Return: Always 0.
+ */
+
+int main (void)
+{	
+	int n = '0';
+       	while (n <= '9')
+	{
+		putchar(n++);
+		if (n != 58)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
