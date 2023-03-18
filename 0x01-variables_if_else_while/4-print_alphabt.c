6e4444060563ee8bc45d9b4a49f8a179dfccf654
modev34 <modev34@gmail.com>
2023-03-17 17:38:55 -0700

    f

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints alphapet in lowercase and uppercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char small = 'a';
+
+	while (small <= 'z')
+	{
+		if(small == 'e' || small == 'q')
+		{
+			small++;
+			continue;
+		}
+		putchar(small);
+		small++;
+	}
+	putchar('\n');
+	return (0);
+}
+
