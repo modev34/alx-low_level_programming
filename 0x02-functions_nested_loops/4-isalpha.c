6e2c24d5a425b7e68c897ad0af1a14b59e1aaa24
modev34 <modev34@gmail.com>
2023-03-21 16:16:09 -0700

    f

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _isalpha - check if letter is aplphabet or not.
+ * @c -  is the argument that the function checks
+ * Return: 0 if non-alpabet and 1 if otherwise
+ */
 int _isalpha(int c)
 {
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
