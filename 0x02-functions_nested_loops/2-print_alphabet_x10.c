5d78bd849d5aa01a78469e16a55352f2429f3c6c
modev34 <modev34@gmail.com>
2023-03-21 15:07:37 -0700

    t3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,11 @@
  * print_alphabet_x10 - print alphabet 10 times.
  */
 
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int i = 0;
 
-	while (i >= 10)
+	while (i <= 10)
 	{
 		print_alphabet();
 		i++;
