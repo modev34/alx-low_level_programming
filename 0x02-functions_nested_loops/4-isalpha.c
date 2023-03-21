cdc182c9fd66518c60d84db4d0fe45677ae4c8ab
modev34 <modev34@gmail.com>
2023-03-21 16:17:19 -0700

    f

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - check if letter is aplphabet or not.
- * @c -  is the argument that the function checks
+ * @c:  is the argument that the function checks
  * Return: 0 if non-alpabet and 1 if otherwise
  */
 int _isalpha(int c)
