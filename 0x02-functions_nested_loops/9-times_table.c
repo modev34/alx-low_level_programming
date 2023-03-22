515a8d360c48e3dacf3427ca8c3676b849f0506c
modev34 <modev34@gmail.com>
2023-03-21 18:59:45 -0700

    f

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= n; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			int product = i * j;
 
