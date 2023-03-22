f3efcaa360c57ef1f138e41438226bac93a80bf7
modev34 <modev34@gmail.com>
2023-03-21 18:23:30 -0700

    t

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @n: number that will be computed
+ * Return: last digit of the computed number of (n)
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
