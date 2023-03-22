760708b51afea78ed32ff8856fb8c3ad0c639d92
modev34 <modev34@gmail.com>
2023-03-21 18:43:41 -0700

    f

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@
 void jack_bauer(void)
 {
 	int hours, mint;
-	for(hours = 0; hours < 24; hours++)
+	for (hours = 0; hours < 24; hours++)
 	{
-		for(mint = 0; mint < 60; mint++)
+		for (mint = 0; mint < 60; mint++)
 		{
 			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
