cbbbb99fbee92f1dd0d087e532492390252a8e45
modev34 <modev34@gmail.com>
2023-03-21 18:14:39 -0700

    t

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * abs - return the absulute value of a number
+ * @n: is the number that will be computed
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	int result;
+	if (n > 0)
+	{
+		result = n * 1;
+	} else if (n < 0)
+	{
+		result = n * -1;
+	} else
+	{
+		result = 0;
+	}
+	return result;
+}
