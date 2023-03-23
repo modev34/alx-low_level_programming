c1aa3da361f9573c7477b082b247d28b9ddd01da
modev34 <modev34@gmail.com>
2023-03-23 15:08:03 -0700

    f

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * main -  test function
- * Return: 0
- *
- */
+* main - Test function for positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
 	i = 0;
 	positive_or_negative(i);
-	return(0);
+
+	return (0);
 }
