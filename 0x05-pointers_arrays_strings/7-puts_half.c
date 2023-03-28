8f6d2f13018f71e95982f0bc9d751a27a0488a98
modev34 <modev34@gmail.com.com>
2023-03-28 03:36:48 +0200

    f

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,27 @@
  */
 void puts_half(char *str)
 {
-	int length = 0, i;
-	int start = (length - 1) / 2 + 1;
+	int length = 0;
+	int i, start;
 
 	while (str[length] != '\0')
+	{
 		length++;
+	}
 
-	for (i = start; str[i] != '\0'; i++)
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+	{
+		start = (length - 1) / 2;
+	}
+
+	for (i = start; i < length; i++)
+	{
 		_putchar(str[i]);
-
+	}
 	_putchar('\n');
 }
+
