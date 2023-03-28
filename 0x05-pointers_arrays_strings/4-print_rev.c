812590b285fe6b63f1d10038d4f7c4518dabb1b8
modev34 <modev34@gmail.com.com>
2023-03-28 02:55:28 +0200

    f

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,16 @@
 void print_rev(char *s)
 {
 	int len = 0;
+	int i = len - 1;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	while (len - 1 >= 0)
+	while (i >= 0)
 	{
-		_putchar(s[len]);
-		len--;
+		_putchar(s[i]);
+		i--;
 	}
 	_putchar('\n');
 }
