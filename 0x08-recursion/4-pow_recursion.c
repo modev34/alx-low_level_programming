bd8a3da67503d1b63c5efeee3179f3c1f821f163
modev34 <modev34@gmail.com>
2023-04-07 04:02:10 +0200

    t

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,21 +8,22 @@
 
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-        return (-1);
-    }
-    else if (y == 0)
-    {
-        return (1);
-    }
-    else if (y % 2 == 0)
-    {
-        int tmp = _pow_recursion(x, y/2);
-        return tmp * tmp;
-    }
-    else
-    {
-        return x * _pow_recursion(x, y-1);
-    }
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y % 2 == 0)
+	{
+		int tmp = _pow_recursion(x, y / 2);
+
+		return tmp * tmp;
+	}
+	else
+	{
+		return x * _pow_recursion(x, y - 1);
+	}
 }
